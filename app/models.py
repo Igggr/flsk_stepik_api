9b1aa3c944ed398ad98c1af69b8a209d5d5e4bad
Iggr Smirnov <uchetzapis@yahoo.com>
2020-02-26 13:52:06 +0300

    add password hashing

--- app/models.py
+++ app/models.py
@@ -1,7 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from werkzeug.security import check_password_hash, generate_password_hash
+import enum
 
 db = SQLAlchemy()
+
+
+class TypeEnum(enum.Enum):
+    HACKATON = 1
+    MEETUP = 2
+    LECTURE = 3
 
 
 class Event(db.Model):
@@ -21,6 +28,19 @@ class Event(db.Model):
     location = db.relationship("Location", back_populates='events')
     enrollments = db.relationship("Enrollment", back_populates='event')
 
+    TypeEnum = TypeEnum
+
+    def __str__(self):
+        return f"{self.title}"
+
+    @property
+    def type(self):
+        self._type = type
+
+    @type.setter
+    def type(self, type_enum):
+        self._type = type_enum
+
 
 class Participant(db.Model):
     __tablename__ = 'participants'
@@ -33,6 +53,20 @@ class Participant(db.Model):
     about = db.Column(db.String)
 
     enrollments = db.relationship("Enrollment", back_populates='participant')
+
+    def __str__(self):
+        return f"{self.name}, {self.email}"
+
+    @property
+    def password(self):
+        raise ValueError('Не положено знать')
+
+    @password.setter
+    def password(self, plaintext):
+        self._password = generate_password_hash(plaintext)
+
+    def check_password(self, plaintext):
+        return check_password_hash(plaintext, self._password)
 
 
 class Enrollment(db.Model):
@@ -54,3 +88,6 @@ class Location(db.Model):
     code = db.Column(db.String)
 
     events = db.relationship(Event, back_populates='location')
+
+    def __str__(self):
+        return f"{self.title}, {self.code}"
